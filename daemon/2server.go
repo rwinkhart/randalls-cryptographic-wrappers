5369c3aa44ef901f92c17a63443d4f77bff15ebe
Randall Winkhart <idgr@tutanota.com>
2025-04-04 21:59:46 -0400

    Fix pidToPath on FreeBSD

--- daemon/2server.go
+++ daemon/2server.go
@@ -99,7 +99,7 @@ func handleConn(conn net.Conn) {
 
 // pidToPath returns the path of the executable that has the given PID.
 func pidToPath(pid int) string {
-	path, _ := os.Readlink(fmt.Sprintf("/proc/%d/exe", pid))
+	path, _ := os.Readlink(fmt.Sprintf("/proc/%d/"+pidPathFile, pid))
 	return path
 }
 
---
+++ daemon/2serverFreeBSD.go
@@ -0,0 +1,5 @@
+//go:build freebsd
+
+package daemon
+
+const pidPathFile = "file"
